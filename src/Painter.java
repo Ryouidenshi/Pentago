31763d71db296284957980374d392df26b69baef
Evgeniy <djonich@mail.ru>
2021-03-16 01:14:50 +0500

    DONE

--- src/Painter.java
+++ src/Painter.java
@@ -6,21 +6,7 @@ public class Painter {
         cleaner.Clear(0);
         for (int i = 0; i < board.getMatrix().getColumns(); i++) {
             for (int j = 0; j < board.getMatrix().getRows(); j++) {
-                if (balls[i][j].getColor() == Color.None) {
-                    System.out.print('·' + " ");
-                }
-
-                if (balls[i][j].getColor() == Color.Black) {
-                    System.out.print('●' + " ");
-                }
-
-                if (balls[i][j].getColor() == Color.White) {
-                    System.out.print('○' + " ");
-                }
-
-                if (j + 1 == board.getMatrix().getRows()) {
-                    System.out.println();
-                }
+                HelpDraw(balls, i, j, null, board);
             }
         }
         cleaner.Clear(0);
@@ -32,23 +18,30 @@ public class Painter {
         cleaner.Clear(0);
         for (int i = 0; i < childBoard.getMatrix().getColumns(); i++) {
             for (int j = 0; j < childBoard.getMatrix().getRows(); j++) {
-                if (balls[i][j].getColor() == Color.None) {
-                    System.out.print('·' + " ");
-                }
-
-                if (balls[i][j].getColor() == Color.Black) {
-                    System.out.print('●' + " ");
-                }
-
-                if (balls[i][j].getColor() == Color.White) {
-                    System.out.print('○' + " ");
-                }
-
-                if (j + 1 == childBoard.getMatrix().getRows()) {
-                    System.out.println();
-                }
+                HelpDraw(balls, i, j, childBoard, null);
             }
         }
         cleaner.Clear(0);
+    }
+
+    public void HelpDraw(Ball[][] balls, int i, int j, ChildBoard childBoard, Board board) {
+        if (balls[i][j].getColor() == Color.None) {
+            System.out.print('·' + " ");
+        }
+
+        if (balls[i][j].getColor() == Color.Black) {
+            System.out.print('●' + " ");
+        }
+
+        if (balls[i][j].getColor() == Color.White) {
+            System.out.print('○' + " ");
+        }
+
+        if (childBoard!=null && j + 1 == childBoard.getMatrix().getRows()) {
+            System.out.println();
+        }
+        if (board!=null && j + 1 == board.getMatrix().getRows()) {
+            System.out.println();
+        }
     }
 }
